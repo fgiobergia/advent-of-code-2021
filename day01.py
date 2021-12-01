6a6f762e2f219167cdcf0f622d8f047d40a6546f
Flavio Giobergia <flavio.giobergia@gmail.com>
2021-12-02 00:07:33 +0100

    minor refactoring

--- day01.py
+++ day01.py
@@ -1,8 +1,6 @@
 with open("day01.input") as f:
     depths = list(map(int, f.readlines()))
 
-print(sum(1 for i in range(1, len(depths)) if depths[i] > depths[i-1]))
+print(sum(depths[i] > depths[i-1] for i in range(1, len(depths))))
 
-# reaching len(depths) instead of len(depths)-N b/c "overflowing" sums
-# will never be larger than the final "full" sum (since all values are > 0
-print(sum(1 for i in range(len(depths)) if sum(depths[i:i+3]) < sum(depths[i+1:i+4])))
+print(sum(sum(depths[i:i+3]) < sum(depths[i+1:i+4]) for i in range(len(depths)-3)))
