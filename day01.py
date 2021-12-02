663285aa656853f6a57162ef7861836ffe2bf9ce
Flavio Giobergia <flavio.giobergia@gmail.com>
2021-12-02 12:18:06 +0100

    added main

--- day01.py
+++ day01.py
@@ -1,6 +1,7 @@
-with open("day01.input") as f:
-    depths = list(map(int, f.readlines()))
+if __name__ == "__main__":
+    with open("day01.input") as f:
+        depths = list(map(int, f.readlines()))
 
-print(sum(depths[i] > depths[i-1] for i in range(1, len(depths))))
+    print(sum(depths[i] > depths[i-1] for i in range(1, len(depths))))
 
-print(sum(sum(depths[i:i+3]) < sum(depths[i+1:i+4]) for i in range(len(depths)-3)))
+    print(sum(sum(depths[i:i+3]) < sum(depths[i+1:i+4]) for i in range(len(depths)-3)))
