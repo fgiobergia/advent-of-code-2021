f28af367ec72cdcc182186f95821460c9e63b490
Flavio Giobergia <flavio.giobergia@gmail.com>
2021-12-07 09:42:11 +0100

    faster part 2 (numpy ftw)

--- solutions/day07.py
+++ solutions/day07.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def sum_n(n):
-    return (n+1)*n//2
+    return (abs(n)+1)*abs(n)//2
 
 if __name__ == "__main__":
     with open("day07.input") as f:
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     
     print(int(abs(pos - np.median(pos)).sum()))
 
-    print(np.min([ sum([ sum_n(abs(pos[i] - k)) for i in range(len(pos)) ]) for k in range(pos.max()) ]))
+    f = np.vectorize(sum_n)
+    print(f(pos - pos.reshape(-1,1)).sum(axis=1).min())
