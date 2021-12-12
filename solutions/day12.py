5764043d42db361976e26cd389a27307a0736bd7
Flavio Giobergia <flavio.giobergia@gmail.com>
2021-12-12 21:24:58 +0100

    1m30s to 1.2s -- sets ftw!

--- solutions/day12.py
+++ solutions/day12.py
@@ -3,16 +3,16 @@ import networkx as nx
 def visit(G, visited_list, current_pos, currently_visited, twice):
     if current_pos == "end":
         if currently_visited not in visited_list:
-            visited_list.append(currently_visited)
+            visited_list.add(currently_visited)
         return
     
     for neighbor in G.adj[current_pos]:
         if (neighbor.islower() and neighbor not in currently_visited) or neighbor.isupper():
             # can visit, go there!
-            visit(G, visited_list, neighbor, currently_visited + [ neighbor ], twice)
+            visit(G, visited_list, neighbor, currently_visited + (neighbor,), twice)
         elif not twice and neighbor.islower() and neighbor in currently_visited and neighbor != "start":
             # use "visit twice" token
-            visit(G, visited_list, neighbor, currently_visited + [ neighbor ], True)
+            visit(G, visited_list, neighbor, currently_visited + (neighbor,), True)
 
 
 if __name__ == "__main__":
@@ -23,10 +23,10 @@ if __name__ == "__main__":
     for a,b in lines:
         G.add_edge(a,b)
     
-    visited_list = []
-    visit(G, visited_list, "start", ["start"], True)
+    visited_list = set()
+    visit(G, visited_list, "start", ("start",), True)
     print(len(visited_list))
     
-    visited_list = []
-    visit(G, visited_list, "start", ["start"], False)
-    print(len(visited_list)) # sit back, relax, and enjoy the wait
+    visited_list = set()
+    visit(G, visited_list, "start", ("start",), False)
+    print(len(visited_list))
